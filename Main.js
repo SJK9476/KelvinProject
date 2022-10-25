77d5f1ec2054cd56448376a8858ba443cb16f6e2
SJK9476 <116685364+SJK9476@users.noreply.github.com>
2022-10-25 22:26:43 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,26 @@
+// Kelvin Project
+
+const kelvin = 580;
+// 1 kelvin will always equal 293
+
+var celsius = kelvin - 273;
+// To convert Kelvin to celsius you need to subtract 273
+
+var fahrenheit = (celsius * ((9/5) + 32));
+// equation to calculate temp in fahrenheit
+
+fahrenheit = Math.floor(fahrenheit);
+// function used to round result down to nearest whole number
+
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
+// Code to insert the fahrenhiet variable value into the middle of a string of text.
+
+var newton = (celsius * (33 / 100))
+// conversion equation for temperature in Newton's.
+
+newton = Math.floor(newton);
+// Used to round result down to closest whole number.
+
+console.log(`The temperature is ${newton} degrees Newton.`);
+// Displays string stating temperature in Newton's.
+
